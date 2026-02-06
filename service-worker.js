382664e3dbc7bfdec42ebcb6c0c3d55f081d5df0
gulinodesign-maker <gulinodesign@gmail.com>
2026-02-06 21:07:18 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-/* AMF_1.086 */
-const CACHE_NAME='AMF_1.086';
+/* AMF_1.089 */
+const CACHE_NAME='AMF_1.089';
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=1.086",
+  "./styles.css?v=1.089",
   "./config.js",
-  "./app.js?v=1.086",
+  "./app.js?v=1.089",
   "./manifest.json",
   "./version.json"
 ];
