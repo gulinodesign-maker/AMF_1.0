9b8d15cf9de712adc46e1938d1dbd86200aaf584
gulinodesign-maker <gulinodesign@gmail.com>
2026-02-28 09:08:52 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-/* AMF_1.137 */
+/* AMF_1.138 */
 (async () => {
-    const BUILD = "AMF_1.137";
-    const DISPLAY = "1.137";
+    const BUILD = "AMF_1.138";
+    const DISPLAY = "1.138";
 
 
     const STANDALONE = true; // Standalone protetto (nessuna API remota)
@@ -6161,7 +6161,7 @@ async function renderSocietaDeleteList() {
   // PWA (iOS): registra Service Worker
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("./service-worker.js?v=1.137").catch(() => {});
+      navigator.serviceWorker.register("./service-worker.js?v=1.138").catch(() => {});
     });
   }
 })();
