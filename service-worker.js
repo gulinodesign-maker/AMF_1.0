6e718cb5a7884cbab8b5d4a4f1702f2ffc2b8540
gulinodesign-maker <gulinodesign@gmail.com>
2026-03-02 12:33:01 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-/* AMF_1.164 */
-const CACHE_NAME='AMF_1.164';
+/* AMF_1.166 */
+const CACHE_NAME='AMF_1.166';
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=1.164",
+  "./styles.css?v=1.166",
   "./config.js",
-  "./app.js?v=1.164",
+  "./app.js?v=1.166",
   "./assets/logo.jpg",
   "./assets/apple-touch-icon.png",
   "./assets/icon-192.png",
